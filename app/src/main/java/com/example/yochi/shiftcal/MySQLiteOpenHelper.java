b2d36cc509363b05438482b4cfd4613b03dbb2f3
yochiboo <yochiboo@gmail.com>
2017-01-30 02:02:56 +0900

    add create tables

--- app/src/main/java/com/example/yochi/shiftcal/MySQLiteOpenHelper.java
+++ app/src/main/java/com/example/yochi/shiftcal/MySQLiteOpenHelper.java
@@ -1,6 +1,7 @@
 static final String DB = "shiftCal.db";
 static final int DB_VERSION = 1;
-static final String CREATE_TABLE = "create table mytable ( _id integer primary key autoincrement, data integer not null );";
+static final String CREATE_TABLE_CAL = "create table mytable (_id integer primary key autoincrement, date numeric not null, shift integer not null);";
+static final String CREATE_TABLE_SHIFT = "create table mytable (_id integer primary key autoincrement, shift integer not null, title text not null, color text not null, order integer not null);";
 static final String DROP_TABLE = "drop table mytable;";
 
 private static class MySQLiteOpenHelper extends SQLiteOpenHelper {
@@ -8,7 +9,8 @@ private static class MySQLiteOpenHelper extends SQLiteOpenHelper {
         super(c, DB, null, DB_VERSION);
     }
     public void onCreate(SQLiteDatabase db) {
-        db.execSQL(CREATE_TABLE);
+        db.execSQL(CREATE_TABLE_CAL);
+        db.execSQL(CREATE_TABLE_SHIFT);
     }
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
         db.execSQL(DROP_TABLE);
