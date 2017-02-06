ef9cfb879fdcf71ed39ff6b7f58cbd1c4e0d7b10
yochiboo <yochiboo@gmail.com>
2017-02-07 07:59:55 +0900

    add dao

--- app/src/main/java/com/example/yochi/shiftcal/DateUtil.java
+++ app/src/main/java/com/example/yochi/shiftcal/DateUtil.java
@@ -1,6 +1,15 @@
+package com.example.yochi.shiftcal;
+
 class DateUtil extends BaseAdapter {
   // カレンダーTOP日付取得
+  public Timestamp getMonthlyCalendarTop(Timestamp month){
+
+  }
   // カレンダーLAST日付取得
+  public Timestamp getMonthlyCalendarLast(Timestamp month){
+      Timestamp top = getMonthlyCalendarTop(month);
+      
+  }
   // 祝祭日リスト取得
   // 祝祭日リスト取得（振替対応）
 
