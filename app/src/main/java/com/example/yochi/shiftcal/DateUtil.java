d4661e6b40f52ca6f64ae0aab510b7ec475beef1
yochiboo <yochiboo@gmail.com>
2017-02-08 00:28:38 +0900

    add getMonthlyCalendarTop, getMonthlyCalendarLast

--- app/src/main/java/com/example/yochi/shiftcal/DateUtil.java
+++ app/src/main/java/com/example/yochi/shiftcal/DateUtil.java
@@ -1,20 +1,34 @@
-class DateUtil extends BaseAdapter {
-class DateUtil extends BaseAdapter {
-class DateUtil extends BaseAdapter {
-class DateUtil extends BaseAdapter {
-class DateUtil extends BaseAdapter {
 package com.example.yochi.shiftcal;
 
+import java.sql.Timestamp;
+import java.util.Calendar;
+
 class DateUtil {
->>>>>>> 08c561eb46bf6d64def8800bd71fccf178c0a4d4
+
+  static final int MONTHLY_CALENDAR_DAY_NUM = 42;
+
+
   // カレンダーTOP日付取得
   public Timestamp getMonthlyCalendarTop(Timestamp month){
+      Calendar top = Calendar.getInstance();
+      top.setTime(month);
 
+      // 注．カレンダーは日曜日(daypart=1)はじまり
+      int daypart = top.get(Calendar.DAY_OF_WEEK);
+      top.add(Calendar.DAY_OF_MONTH, -(daypart-1));
+      return (Timestamp) top.getTime();
   }
+
   // カレンダーLAST日付取得
   public Timestamp getMonthlyCalendarLast(Timestamp month){
-      Timestamp top = getMonthlyCalendarTop(month);
 
+      Timestamp top = getMonthlyCalendarTop(month);
+      Calendar last = Calendar.getInstance();
+      last.setTime(top);
+
+      // 注．カレンダーTOP日付の42日後
+      last.add(Calendar.DAY_OF_MONTH, MONTHLY_CALENDAR_DAY_NUM);
+      return (Timestamp) last.getTime();
   }
   // 祝祭日リスト取得
   // 祝祭日リスト取得（振替対応）
