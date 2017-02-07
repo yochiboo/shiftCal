e2106e952d94c924c29e37e721917d4e2c8e5365
yochiboo <yochiboo@gmail.com>
2017-02-08 01:00:45 +0900

    edit shiftDao

--- app/src/main/java/com/example/yochi/shiftcal/ShiftDao.java
+++ app/src/main/java/com/example/yochi/shiftcal/ShiftDao.java
@@ -2,62 +2,48 @@ package com.example.yochi.shiftcal;
 
 import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
+import java.sql.Timestamp;
+import java.util.ArrayList;
+
 class ShiftDao {
 
+    public Long date = null;
+    public Integer shift = null;
+
+    private Context context = null;
     private SQLiteDatabase mydb = null;
 
-    public MySQLiteOpenHelper(Context c) {
+    public ShiftDao() {
+    }
+    public ShiftDao(Context c) {
+        context = c;
     }
 
     public ArrayList<ShiftDao> getMonthlyShift(Timestamp from, Timestamp to){
-        String format = "select date, shift from cal where date bitween %s and %s";
-        String sql = String.format(format, top, bottom);
-        Cursor c = openDb().rawQuery(sql);
-        f(c.moveToFirst()){
+        ArrayList<ShiftDao> result = new ArrayList<ShiftDao>();
+        String sql = "select date, shift from cal where date bitween ? and ?";
+        Cursor c = openDb().rawQuery(sql, new String[]{Long.toString(from.getTime()), Long.toString(to.getTime())});
+        if(c.moveToFirst()){
           do{
-            long date = c.getLong(c.getColumnIndex("date"));
-            long shift = c.getLong(c.getColumnIndex("shift"));
+            ShiftDao s = new ShiftDao();
+            s.date = c.getLong(c.getColumnIndex("date"));
+            s.shift = c.getInt(c.getColumnIndex("shift"));
+            result.add(s);
           }while(c.moveToNext());
         }
-
+        return reslt;
     }
 
     private SQLiteDatabase openDb(){
         if(mydb == null){
-            MySQLiteOpenHelper hlpr = new MySQLiteOpenHelper(mContext);
+            MySQLiteOpenHelper hlpr = new MySQLiteOpenHelper(context);
             mydb = hlpr.getWritableDatabase();
         }
         return mydb;
     }
 
-    public void onCreate(SQLiteDatabase db) {
-        db.execSQL(CREATE_TABLE_CAL);
-        db.execSQL(CREATE_TABLE_SHIFT);
-
-        // shiftテーブル初期データ投入
-        ContentValues values = new ContentValues();
-        values.put("shift", 1);
-        values.put("title", "早番");
-        values.put("color", "#ff0000ff");
-        values.put("order", 1);
-        db.insert("shift", null, values);
-        values.put("shift", 2);
-        values.put("title", "日勤");
-        values.put("color", "#ff00ff00");
-        values.put("order", 2);
-        db.insert("shift", null, values);
-        values.put("shift", 3);
-        values.put("title", "遅番");
-        values.put("color", "#ffff0000");
-        values.put("order", 3);
-        db.insert("shift", null, values);
-    }
-
-    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-        //db.execSQL(DROP_TABLE);
-        //onCreate(db);
-    }
 }
