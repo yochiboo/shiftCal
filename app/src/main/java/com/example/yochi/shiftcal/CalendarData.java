ebfaf78ea0f2964ec7255caabd86ceabde519ee7
yochiboo <yochiboo@gmail.com>
2017-02-11 18:18:07 +0900

    add dao folder

---
+++ app/src/main/java/com/example/yochi/shiftcal/CalendarData.java
@@ -0,0 +1,8 @@
+package com.example.yochi.shiftcal;
+
+class CalendarData {
+    public Long date = null;
+    public Integer shift = null;
+    public String title = "";
+    public String color = "";
+}
